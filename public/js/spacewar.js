1312764bdfe9eff9b721a2a8971aa00f98ed27a5
Adam Reis <adamhreis@gmail.com>
2016-11-13 16:46:51 -0800

    spacewar MVP

--- public/js/spacewar.js
+++ public/js/spacewar.js
@@ -1,67 +1,170 @@
 'use strict';
 
+const numCollisionRows = 4;
+const numCollisionColumns = 6;
+
 var then;
 var ctx;
 var players = {};
 var bullets = [];
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'black', 'gray'];
 var canvas;
+var collisionSections;
 
+class Circle {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
 
-class Player {
-  constructor() {
+  get sectionIndices() {
+    return [sectionForCoordinate(this.x, this.y)];
+  }
+
+  intersects(other) {
+    return this.distanceTo(other) < this.r + other.r;
+  }
+
+  distanceTo(other) {
+    var a = this.x - other.x;
+    var b = this.y - other.y;
+    return Math.sqrt(a*a + b*b);
+  }
+}
+
+class Player extends Circle {
+  constructor(id) {
+    let p = getRandomPosition();
+    super(p.x, p.y, 15);
+
     this.color = colors.pop();
     this.theta = Math.random() * Math.PI * 2;
     this.score = 0;
     this.speed = 100; // px/second
-    let p = getRandomPosition();
-    this.x = p.x;
-    this.y = p.y;
+    this.id = id;
   }
+
   render(ctx) {
-    const R = 15;
     // Stroke circle.
     ctx.beginPath();
-    ctx.arc(this.x, this.y, R,0,2*Math.PI);
+    ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
     ctx.strokeStyle = "black";
     ctx.lineWidth = 2;
     ctx.stroke();
+
     // Fill circle.
     ctx.beginPath();
     ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, R,0,2*Math.PI);
+    ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
     ctx.fill();
+
     // Triangle
     ctx.beginPath();
     ctx.fillStyle = 'black';
     // TODO: fix these.
-    let h = R*2;
-    ctx.moveTo(this.x + R * Math.cos(this.theta - Math.PI/2), this.y + R * Math.sin(this.theta - Math.PI/2));
+    let h = this.r * 2;
+    ctx.moveTo(this.x + this.r * Math.cos(this.theta - Math.PI/2), this.y + this.r * Math.sin(this.theta - Math.PI/2));
     ctx.lineTo(this.x + h * Math.cos(this.theta), this.y + h * Math.sin(this.theta));
-    ctx.lineTo(this.x + R * Math.cos(this.theta + Math.PI/2), this.y + R * Math.sin(this.theta + Math.PI/2));
+     ctx.lineTo(this.x + this.r * Math.cos(this.theta + Math.PI/2), this.y + this.r * Math.sin(this.theta + Math.PI/2));
     ctx.stroke();
   }
-  update() {
+
+  update(modifier) {
+    if (this.a) { // Boost!
+      this.speed = 200;
+    } else {
+      this.speed = 100;
+    }
+    if (this.b) {
+      // Fire weapon.
+      bullets.push(new Bullet(
+        this.x + Math.cos(this.theta) * 40,
+        this.y + Math.sin(this.theta) * 40,
+        // Force velocity to sum to 1.
+        Math.cos(this.theta),
+        Math.sin(this.theta),
+        // Who to give points for a kill.
+        this.id
+      ));
+      // Consume touch so we only fire 1 bullet per press.
+      delete this.b;
+    }
+    if (this.stick) {
+      this.x += this.stick.position.x * this.speed * modifier;
+      this.y += this.stick.position.y * this.speed * modifier;
+      this.theta = Math.atan2(this.stick.position.y, this.stick.position.x);
+    }
+    // Bounds.
+    this.x = Math.min(Math.max(0, this.x), canvas.offsetWidth);
+    this.y = Math.min(Math.max(0, this.y), canvas.offsetHeight);
+    // TODO: if intersecting a bullet, update score & reset position
+  }
+
+  respawn() {
+    let p = getRandomPosition();
+    this.x = p.x;
+    this.y = p.y;
+    this.theta = Math.random() * Math.PI * 2;
   }
 }
 
-class Bullet {
+class Bullet extends Circle {
+
   constructor(x, y, vx, vy, owner) {
-    this.x = x;
-    this.y = y;
+    super(x, y, 5);
+
     this.vx = vx;
     this.vy = vy;
     this.owner = owner;
   }
+
   render(ctx) {
-    const R = 5;
     // Fill circle.
     ctx.beginPath();
     ctx.fillStyle = 'red';
-    ctx.arc(this.x, this.y, R,0,2*Math.PI);
+    ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
     ctx.fill();
   }
-  update() {
+
+  update(modifier, index) {
+    this.x += this.vx * 300 * modifier;
+    this.y += this.vy * 300 * modifier;
+
+    // Handle first collision
+    let collided = false;
+    for (var i of this.sectionIndices) {
+      let section = collisionSections[i];
+
+      if (collided) {
+        break;
+      }
+
+      for (var player of section.players) {
+        if (this.intersects(player)) {
+          collided = true;
+          players[this.owner].score++;
+          player.respawn();
+          break;
+        }
+      }
+
+      for (var bullet of section.bullets) {
+        if (this.x == bullet.x && this.y == bullet.y && this.owner == bullet.owner) {
+          continue;
+        }
+        if (this.intersects(bullet)) {
+          collided = true;
+        }
+      }
+    }
+
+    // Despawn bullets that have gone off the map.
+    // Consider tracking bullet travel distance and capping range.
+    let isOutOfBounds = this.x < 0 || this.x > canvas.offsetWidth || this.y < 0 || this.y > canvas.offsetHeight;
+    if (isOutOfBounds || collided) {
+      bullets.splice(index, 1);
+    }
   }
 }
 
@@ -81,49 +184,47 @@ document.addEventListener("DOMContentLoaded", function(event) {
   main();
 });
 
-// Update positions.
-function update(modifier) {
-  // TODO: move this logic into the objects.
+function blankCollisionSections() {
+  var sections = new Array();
+  for (var i = 0; i < numCollisionRows * numCollisionColumns; i++) {
+    sections.push({
+      'players': [],
+      'bullets': []
+    });
+  }
+  return sections;
+}
+
+function createCollisionSections() {
+  var newSections = blankCollisionSections();
+
   for (var i = bullets.length - 1; i >= 0; i--) {
     let bullet = bullets[i];
-    bullet.x += bullet.vx * 300 * modifier;
-    bullet.y += bullet.vy * 300 * modifier;
-    // Despawn bullets that have gone off the map.
-    // Consider tracking bullet travel distance and capping range.
-    if (bullet.x < 0 || bullet.x > canvas.offsetWidth || bullet.y < 0 || bullet.y > canvas.offsetHeight) {
-      bullets.splice(i, 1);
-    }
+    bullet.sectionIndices.forEach(function(i) {
+      newSections[i]['bullets'].push(bullet);
+    });
   }
+
   for (var id in players) {
     let player = players[id];
-    if (player.a) { // Boost!
-      player.speed = 200;
-    } else {
-      player.speed = 100;
-    }
-    if (player.b) {
-      // Fire weapon.
-      bullets.push(new Bullet(
-        player.x + Math.cos(player.theta) * 40,
-        player.y + Math.sin(player.theta) * 40,
-        // Force velocity to sum to 1.
-        Math.cos(player.theta),
-        Math.sin(player.theta),
-        // Who to give points for a kill.
-        id
-      ));
-      // Consume touch so we only fire 1 bullet per press.
-      delete player.b;
-    }
-    if (player.stick) {
-      player.x += player.stick.position.x * player.speed * modifier;
-      player.y += player.stick.position.y * player.speed * modifier;
-      player.theta = Math.atan2(player.stick.position.y, player.stick.position.x);
-    }
-    // Bounds.
-    player.x = Math.min(Math.max(0, player.x), canvas.offsetWidth);
-    player.y = Math.min(Math.max(0, player.y), canvas.offsetHeight);
-    // TODO: if intersecting a bullet, update score & reset position
+    player.sectionIndices.forEach(function(i) {
+      newSections[i]['players'].push(player);
+    });
+  }
+
+  return newSections;
+}
+
+// Update positions.
+function update(modifier) {
+  collisionSections = createCollisionSections();
+
+  for (var i = bullets.length - 1; i >= 0; i--) {
+    bullets[i].update(modifier, i);
+  }
+
+  for (var id in players) {
+    players[id].update(modifier);
   }
 }
 
@@ -176,13 +277,25 @@ onMessageReceived = function(x) {
   } else {
     player[x.value] = x;
   }
-}
+};
 
 onConnected = function(id) {
   console.log(id, 'connected');
   sendToClient(id, {hello: 'client'});
   if (!players[id]) {
-    players[id] = new Player();
+    players[id] = new Player(id);
   }
-}
+};
 
+function sectionForCoordinate(x, y) {
+  x = Math.max(0, Math.min(x, canvas.offsetWidth - 1));
+  y = Math.max(0, Math.min(y, canvas.offsetHeight - 1));
+
+  let rowHeight = canvas.offsetHeight / numCollisionRows;
+  let columnWidth = canvas.offsetWidth / numCollisionColumns;
+
+  let row = Math.floor(y / rowHeight);
+  let column = Math.floor(x / columnWidth);
+
+  return column + row * (numCollisionColumns);
+};
