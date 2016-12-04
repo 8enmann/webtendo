5e674380bfe32498a9657ef90e47bb2ef9d44e7e
Benjamin Mann <8enmann@gmail.com>
2016-12-03 19:59:11 -0800

    add TURN servers for punching NAT

--- public/out/spacewar-client-bundle.js
+++ public/out/spacewar-client-bundle.js
@@ -7897,7 +7897,19 @@
 	 ****************************************************************************/
 	
 	var configuration = {
-	  'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }, { 'url': 'stun:stun.services.mozilla.com' }]
+	  'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }, { 'url': 'stun:stun.services.mozilla.com' }, {
+	    'url': 'turn:192.158.29.39:3478?transport=udp',
+	    'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	    'username': '28224511:1379330808'
+	  }, {
+	    'url': 'turn:192.158.29.39:3478?transport=tcp',
+	    'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	    'username': '28224511:1379330808'
+	  }, {
+	    url: 'turn:numb.viagenie.ca',
+	    credential: 'muazkh',
+	    username: 'webrtc@live.com'
+	  }]
 	};
 	
 	// Create a random room if not already present in the URL.
