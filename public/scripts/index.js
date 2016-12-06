a855c4d643dde0dff255d4b8c2d1882cac7edbdc
Benjamin Mann <8enmann@gmail.com>
2016-12-05 20:10:33 -0800

    Remove generated files from git, commit index.js

---
+++ public/scripts/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import io from 'socket.io-client';
+
+let socket = io.connect();
+let el = document.getElementById('room');
+socket.on('room', room => {
+  el.value = room;
+  window.location.hash = room;
+});
+
+// Ask server for default room.
+if (!window.location.hash) {
+  socket.emit('get room');
+} else {
+  // Strip off leading hash.
+  el.value = window.location.hash.slice(1);
+}
+
+// Let user manually override.
+el.addEventListener("input", e => {
+  window.location.hash = e.target.value.toLowerCase();
+});
+
+// Preserve hash on navigation.
+let links = document.getElementsByTagName('a');
+for (let i = 0; i < links.length; i++) {
+  let link = links[i];
+  if (!link.dataset.game) {
+    continue;
+  }
+  let ontouch = e => {
+    window.location.pathname = `/${link.dataset.game}/${link.dataset.role}.html`;
+  }
+  // Both mobile and desktop.
+  link.addEventListener('touchend', ontouch);
+  link.addEventListener('click', ontouch);
+}
