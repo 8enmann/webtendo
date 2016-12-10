7fd51bfad6a2d9eb151900b32e328b5ff5ea596e
Benjamin Mann <8enmann@gmail.com>
2016-12-10 00:10:47 -0800

    autotrack not working, try script tag instead

--- public/scripts/analytics.js
+++ public/scripts/analytics.js
@@ -6,7 +6,6 @@ export function init() {
   ga('create', 'UA-88759837-1', 'auto');
 
   // Start plugins
-  /*
   ga('require', 'cleanUrlTracker', {
     stripQuery: true,
     indexFilename: 'index.html',
@@ -19,7 +18,7 @@ export function init() {
       nonInteraction: null
     }
   });
-*/
+
   ga('send', 'pageview');
 
 }
